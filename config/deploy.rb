9532bbf4577a55232095e85ba3291b8917d2ecd9
Luis Valenzuela <lvalenzuela@longbourn.cl>
2014-05-16 17:05:08 -0400

    capistrano OK

---
+++ config/deploy.rb
@@ -0,0 +1,50 @@
+require 'bundler/capistrano'
+require "rvm/capistrano"
+
+set :application, "Longbourn Management"
+set :repository,  "git@github.com:lvalenzuela/lb_management.git"
+set :deploy_to, "/home/ubuntu/lb_management"
+set :scm, :git
+set :branch, "master"
+set :user, "ubuntu"
+set :group, "deployers"
+set :use_sudo, false
+set :rails_env, "development"
+set :deploy_via, :copy
+set :ssh_options, { :forward_agent => true }
+set :keep_releases, 5
+default_run_options[:pty] = true
+server "54.237.95.165", :app, :web, :db, :primary => true
+
+set :rvm_ruby_string, :local        # use the same ruby as used locally for deployment
+
+before 'deploy', 'rvm:install_rvm'  # install/update RVM
+before 'deploy', 'rvm:install_ruby' # install Ruby and create gemset (both if missing)
+
+namespace :deploy do
+  task :start do ; end
+  task :stop do ; end
+
+  #desc "Symlink shared config files"
+  #task :symlink_config_files do
+  #  run "#{ sudo } ln -s #{ deploy_to }/shared/config/database.yml #{ current_path }/config/database.yml"
+  #end
+
+  # NOTE: I don't use this anymore, but this is how I used to do it.
+  desc "Precompile assets after deploy"
+  task :precompile_assets do
+    run <<-CMD
+      cd #{ current_path } &&
+      #{ sudo } bundle exec rake assets:precompile RAILS_ENV=#{ rails_env }
+    CMD
+  end
+
+  desc "Restart applicaiton"
+  task :restart do
+    run "#{ try_sudo } touch #{ File.join(current_path, 'tmp', 'restart.txt') }"
+  end
+end
+
+#after "deploy", "deploy:symlink_config_files"
+after "deploy", "deploy:restart"
+after "deploy", "deploy:cleanup"
