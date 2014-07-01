dafdf5fd35226bcc0eecdbea1d35a619fa2cab49
Luis Valenzuela <lvalenzuela@longbourn.cl>
2014-07-01 10:28:14 -0400

    modificacion a capistrano

--- config/deploy.rb
+++ config/deploy.rb
@@ -57,4 +57,5 @@ end
 #after "deploy", "deploy:symlink_config_files"
 #after "deploy", "deploy:precompile_assets"
 #after "deploy", "deploy:restart"
+after "deploy:update_code", "deploy:migrate"
 after "deploy", "deploy:cleanup"
