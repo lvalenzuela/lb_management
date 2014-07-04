1ca17c85bca6557842b7cded4aadbe3c566bd1b0
Luis Valenzuela <lvalenzuela@longbourn.cl>
2014-07-04 09:56:59 -0400

    ultimos updates

--- db/migrate/20140703134243_create_moodle_groups.rb
+++ db/migrate/20140703134243_create_moodle_groups.rb
@@ -1,3 +1,12 @@
 class CreateMoodleGroups < ActiveRecord::Migration
+  def up
+    create_table :moodle_groups do |t|
+    	t.string :groupname
+      t.timestamps
+    end
+  end
 
+  def down 
+  	drop_table :moodle_groups
+  end
 end
