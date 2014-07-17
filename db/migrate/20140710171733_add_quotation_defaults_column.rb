c365c6a2ce8eb766a3685b896c5e1df925df2823
Luis Valenzuela <lvalenzuela@longbourn.cl>
2014-07-17 12:15:03 -0400

    migration fix

--- db/migrate/20140710171733_add_quotation_defaults_column.rb
+++
@@ -1,9 +0,0 @@
-class AddQuotationDefaultsColumn < ActiveRecord::Migration
-  def up
-  	add_column :quotation_defaults, :default, :integer
-  end
-
-  def down
-  	remove_column :quotation_defaults, :default
-  end
-end
--- db/migrate/20140710190740_add_title_to_formats.rb
+++
@@ -1,9 +0,0 @@
-class AddTitleToFormats < ActiveRecord::Migration
-  def up
-  	add_column :quotation_defaults, :title, :string
-  end
-
-  def down
-  	remove_column :quotation_defaults, :title
-  end
-end
