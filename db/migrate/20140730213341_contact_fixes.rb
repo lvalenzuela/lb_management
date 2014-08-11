d7834ef80896227d22ba8dac055bfd508f4f4f12
Luis Valenzuela <lvalenzuela@longbourn.cl>
2014-08-11 10:41:10 -0400

    mas fixes

--- db/migrate/20140730213341_contact_fixes.rb
+++ db/migrate/20140730213341_contact_fixes.rb
@@ -1,12 +1,12 @@
 class ContactFixes < ActiveRecord::Migration
   def up
-  	remove_column :contacts, :lastname
+  	#remove_column :contacts, :lastname
   	rename_column :contacts, :fistname, :contact_name
   	add_column :contacts, :company_name, :string, after: :contact_name
   end
 
   def down
-  	add_column :contacts, :lastname, :string
+  	#add_column :contacts, :lastname, :string
   	rename_column :contacts, :contact_name, :fistname
   	remove_column :contacts, :company_name
   end
