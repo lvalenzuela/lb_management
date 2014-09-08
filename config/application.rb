6e41c7f7485b8d7e1f62374fe5fb04d102007205
Luis Valenzuela <lvalenzuela@longbourn.cl>
2014-09-08 17:27:07 -0300

    no pude arreglar las fuentes, lo hago despues

--- config/application.rb
+++ config/application.rb
@@ -21,20 +21,5 @@ module LongbournManagement
     # config.i18n.default_locale = :de
     config.i18n.load_path += Dir[Rails.root.join('config', 'locales', '*.{rb,yml}').to_s]
     config.i18n.default_locale = :es
-
-    #para compilar las fuentes
-    initializer 'setup_asset_pipeline', :group => :all  do |app|
-      # We don't want the default of everything that isn't js or css, because it pulls too many things in
-      app.config.assets.precompile.shift
-
-      # Explicitly register the extensions we are interested in compiling
-      app.config.assets.precompile.push(Proc.new do |path|
-        File.extname(path).in? [
-          '.html', '.erb', '.haml',                 # Templates
-          '.png',  '.gif', '.jpg', '.jpeg',         # Images
-          '.eot',  '.otf', '.svc', '.woff', '.ttf', # Fonts
-        ]
-      end)
-    end
   end
 end
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -91,9 +91,4 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
-  
-  #precompile fonts
-  config.assets.precompile << lambda do |filename, path|
-    path =~ /vendor\/assets/ && !%w(.js .css).include?(File.extname(filename))
-  end
 end
