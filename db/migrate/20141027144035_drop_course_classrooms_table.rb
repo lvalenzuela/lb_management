672b88fa2d2daef366a1ba05485ef14cdbfc559e
Luis Valenzuela <lvalenzuela@longbourn.cl>
2014-10-27 12:24:48 -0300

    asdf

--- db/migrate/20141027144035_drop_course_classrooms_table.rb
+++ db/migrate/20141027144035_drop_course_classrooms_table.rb
@@ -1,11 +1,11 @@
 class DropCourseClassroomsTable < ActiveRecord::Migration
   def up
-  	drop_table :course_classrooms
+  	#drop_table :course_classrooms
   end
 
   def down
-  	create_table :course_classrooms do |t|
-  		t.timestamps
-  	end
+  	#create_table :course_classrooms do |t|
+  	#	t.timestamps
+  	#end
   end
 end
